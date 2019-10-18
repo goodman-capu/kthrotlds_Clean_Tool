a99e6d3c73a5c506c342a5d381a531e7670445c8
Frank <goodman.capu@gmail.com>
2019-10-17 20:24:41 -0700

    Update

--- README.md
+++ README.md
@@ -1,35 +1,30 @@
 # 清理工具使用说明
 
-本清理工具适用于2019年2月21日爆发的Watchdogs挖矿蠕虫。
-- Watchdogs挖矿蠕虫主要特征如下：
-   + ps -ef | grep watchdogs               # 存在恶意进程watchdogs
-   + ps -ef | grep ksoftirqds              # 存在恶意进程ksoftirqds
-   + chkconfig | grep watchdogs            # 存在恶意启动项watchdogs
-   + ldd \`which ps\` | grep libioset.so     # ps、rm等命令被so劫持 
-   + crontab -l | grep pastebin            # 存在恶意的蠕虫下载计划任务
-   
-- 使用clear清理Watchdogs步骤：
-   1. 上传busybox到/bin/目录下。
-   2. 运行clear.sh。
+脚本原作者是默安科技（MoreSec），https://github.com/MoreSecLab/DDG_MalWare_Clean_Tool
 
--------------------------------
+本脚本用于处理截至2019年10月的病毒变种。相对于之前的病毒，有如下变化：
 
-# 2019.3.4 新增clear_kthrotlds.sh
+* 病毒实际位于`/usr/bin/[kthrotlds]`
+* crontab内容换汤不换药，具体内容见[cron root.txt](Virus (DO NOT EXECUTE!!!)/cron root.txt)
+* `/etc/cron.hourly`，`/etc/cron.daily`，`/etc/cron.monthly`，`/usr/local/bin/nptd`被病毒感染，具体内容见[cron hourly.txt](Virus (DO NOT EXECUTE!!!)/cron hourly.txt)
+* 存在可疑进程`systemd-logind`和`agetty`，不确定是否和病毒有关
+* 相比之前的清理过程，发现很多文件都被锁住无法修改，故干脆在删除每个文件之前都执行一次`busybox chattr -i`
+* 会向`/root/.ssh/authorized_keys`中写入攻击者的公钥
+*  会向`/etc/ssh/sshd_config`中写入若干配置以允许远程登陆
 
-新增针对2019年3月1日Watchdogs进程变种为kthrotlds的挖矿蠕虫的清理脚本clear_kthrotlds.sh
+## 使用方法
 
-- kthrotlds挖矿蠕虫变种主要特征如下：
-   + watchdogs进程变更为kthrotlds
-   + libioset.so变更为libcset.so
-   + watchdogs开机启动项名称变更为netdns
+* 上传`busybox`到`/bin/`目录下
+* 上传并运行`clear_kthrotlds.sh`
+* 删除`/root/.ssh/authorized_keys`下的攻击者公钥（`ssh-rsa AAAAB...A76WH user@localhost`）
+* 恢复`/etc/ssh/sshd_config`配置，主要查看`PermitRootLogin`，`RSAAuthentication`，`PubkeyAuthentication`，`UsePAM`，`PasswordAuthentication`这五项，以及文件末尾有无多余重复内容
 
-- 使用clear_kthrotlds.sh清理kthrotlds步骤：
-    1. 上传busybox到/bin/目录下。
-    2. 运行clear_kthrotlds.sh。
-    
-# 2019.6.8 补充说明
-木马变种情况建议联系安全厂商介入(如：https://moresec.cn)：
-- 详细分析行为，修改脚本，根除木马。
-- 较难分析情况，重装系统，检查漏洞，修复漏洞，防止再感染。
+## 免责声明
 
-By 默安科技（MoreSec）
+我只是因为个人服务器中毒而研究了这个病毒，没有任何系统安全相关专业知识，分析和解决方案都可能不正确，请使用者自行斟酌！
+
+## 参考网站
+
+* https://www.anquanke.com/post/id/172111
+* https://blog.csdn.net/wo18237095579/article/details/89376744
+* https://cloud.tencent.com/developer/article/1026521
--- clear_kthrotlds.sh
+++ clear_kthrotlds.sh
@@ -1,28 +1,56 @@
 service crond stop
 
+busybox chattr -i /etc/ld.so.preload
 busybox rm -f /etc/ld.so.preload
+busybox chattr -i /usr/local/lib/libcset.so
 busybox rm -f /usr/local/lib/libcset.so
-chattr -i /etc/ld.so.preload
+busybox chattr -i /etc/ld.so.preload
 busybox rm -f /etc/ld.so.preload
+busybox chattr -i /usr/local/lib/libcset.so
 busybox rm -f /usr/local/lib/libcset.so
 
 # 清理异常进程
+busybox ps -ef | busybox grep -v grep | busybox egrep 'systemd-logind' | busybox awk '{print $1}' | busybox xargs kill -9
+busybox ps -ef | busybox grep -v grep | busybox egrep 'agetty' | busybox awk '{print $1}' | busybox xargs kill -9
 busybox ps -ef | busybox grep -v grep | busybox egrep 'ksoftirqds' | busybox awk '{print $1}' | busybox xargs kill -9
 busybox ps -ef | busybox grep -v grep | busybox egrep 'kthrotlds' | busybox awk '{print $1}' | busybox xargs kill -9
 
-busybox rm -f /tmp/kthrotlds
-busybox rm -f /etc/cron.d/tomcat
+busybox chattr -i /etc/cron.d/root
 busybox rm -f /etc/cron.d/root
+busybox chattr -i /var/spool/cron/root
 busybox rm -f /var/spool/cron/root
+busybox chattr -i /var/spool/cron/crontabs/root
 busybox rm -f /var/spool/cron/crontabs/root
+busybox chattr -i /etc/cron.hourly/cronlog
+busybox rm -f /etc/cron.hourly/cronlog
+busybox chattr -i /etc/cron.daily/cronlog
+busybox rm -f /etc/cron.daily/cronlog
+busybox chattr -i /etc/cron.monthly/cronlog
+busybox rm -f /etc/cron.monthly/cronlog
+busybox chattr -i /usr/local/bin/nptd
+busybox rm -f /usr/local/bin/nptd
+
+busybox chattr -i /tmp/kthrotlds
+busybox rm -f /tmp/kthrotlds
+busybox chattr -i /etc/rc.d/init.d/kthrotlds
 busybox rm -f /etc/rc.d/init.d/kthrotlds
+busybox chattr -i /usr/sbin/kthrotlds
 busybox rm -f /usr/sbin/kthrotlds
+busybox chattr -i /usr/bin/[kthrotlds]
+busybox rm -f /usr/bin/[kthrotlds]
+
+busybox chattr -i /etc/init.d/netdns
 busybox rm -f /etc/init.d/netdns
+
+# busybox chattr -i /root/.ssh/authorized_keys
+# busybox rm -f /root/.ssh/authorized_keys
 
 
 ldconfig
 
 # 再次清理异常进程
+busybox ps -ef | busybox grep -v grep | busybox egrep 'systemd-logind' | busybox awk '{print $1}' | busybox xargs kill -9
+busybox ps -ef | busybox grep -v grep | busybox egrep 'agetty' | busybox awk '{print $1}' | busybox xargs kill -9
 busybox ps -ef | busybox grep -v grep | busybox egrep 'ksoftirqds' | busybox awk '{print $1}' | busybox xargs kill -9
 busybox ps -ef | busybox grep -v grep | busybox egrep 'kthrotlds' | busybox awk '{print $1}' | busybox xargs kill -9
 
@@ -31,7 +59,7 @@ chkconfig netdns off
 chkconfig –del netdns
 
 service crond start
-echo "Done, Please reboot!"
-
+echo "Done, Please reboot NOW!"
 
 # sidie@moresec
+# modified by goodman-capu
